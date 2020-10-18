2d23ff3b56cc26c89606e8529a4b8a0cc54ee793
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-18 19:25:39 -0400

    added authenication to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,20 @@ class SessionsController < ApplicationController
     end 
 
     def new
-        
+
+    end 
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+
+        # if @user && @user.authenticate(params[:users][:password])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user_id 
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = 'Sorry, please try again!'
+            redirect_to login_path
+        end 
     end 
 end 
