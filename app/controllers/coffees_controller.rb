9d203c4932b6088612581ffdfa1edb98022a91cb
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-18 21:13:09 -0400

    Created new and create method in coffee controller

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -2,5 +2,22 @@ class CoffeesController < ApplicationController
 
     def new 
         @coffee = Coffee.new 
+        @coffee.build_brand 
     end 
+
+    def create 
+        @coffee = coffee.new(coffee_params)
+        if @coffee.save 
+            redirect_to coffee_path(@coffee)
+        else
+            render :new 
+    end 
+
+
+    private 
+
+    def coffee_params 
+        params.require(coffee).permit(:flavor, :description, :brand_id, brand_attributes: [:name])
+    end 
+
 end
--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,5 +3,5 @@ class Coffee < ApplicationRecord
     belongs_to :user #creator of it 
     has_many :reviews 
     has_many :users, through: :reviews #people who have reviewed it
-    
+    accepts_nested_attributes_for :brand
 end
