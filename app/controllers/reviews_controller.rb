dfbddae27cd5dc09adf9e76c73a6166d6973caf3
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-20 19:41:48 -0400

    created the show route in the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,28 @@
 class ReviewsController < ApplicationController
 
     def new 
-       @coffee =  Coffee.find_by_id(params[:id])
-       @review = @coffee.build_review 
+       @coffee =  Coffee.find_by_id(params[:coffee_id])
+       @review = @coffee.reviews.build 
+    end 
+
+    def create 
+        Review.create(review_params)
+        redirect_to review_path(@review)
+    end 
+
+    def show
+        review belongs_to coffee 
+        @review.coffee  
     end 
 
     def index 
 
+    end 
+
+   
+    private 
+
+    def review_params
+        params.require(:review).permit(:coffee_id, :content, :stars, :title)
     end 
 end
