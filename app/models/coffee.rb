e78dadb9fdb066f3c8bc0e4ff0a3dbc867f51020
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-21 21:54:05 -0400

    added show form to coffees view

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,5 +3,12 @@ class Coffee < ApplicationRecord
     belongs_to :user #creator of it 
     has_many :reviews 
     has_many :users, through: :reviews #people who have reviewed it
-    accepts_nested_attributes_for :brand
+
+    validates :flavor, presense: true 
+
+
+    def brand_attributes=(attributes)
+        brand = Brand.find_or_create_by(attributes) if 
+        !attributes['name'].empty?
+    end 
 end
